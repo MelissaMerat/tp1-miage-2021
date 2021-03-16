b627bc4e6d7f069fe812bf134dcced57db41e1eb
MelissaMerat <melissamerat@gmail.com>
2021-03-16 12:18:14 +0100

    tests ne fonctionnent pas

--- todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
@@ -1,13 +1,47 @@
 package com.acme.todolist;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.acme.todolist.domain.TodoItem;
 
 @SpringBootTest
 class TodolistApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
+//	
+	/**
+	 * RG 1 : si l'item a plus de 24h, ajouter dans le contenu une note "[LATE!]"*/
 
+	@Test
+	void testRPD1LateOK() { 
+		TodoItem ti = new TodoItem("1", Instant.now().minus(25, ChronoUnit.HOURS), "[LATE!] desc item + 24h"); 
+		// RG 1 : si l'item a plus de 24h, le contenu de sa note contient "[LATE!]"
+		assertTrue(ti.finalContent().contains("LATE"));
+		
+	}
+	
+//	@Test
+//	void testRPD1LateNOK() { 
+//		TodoItem ti = new TodoItem("1", Instant.now().minus(1, ChronoUnit.HOURS), "desc item + 24h"); 
+//		// RG 1 : si l'item a moins de 24h, le contenu de sa note ne contient pas "[LATE!]"
+//		assertFalse(ti.finalContent().contains("LATE"));
+//		
+//	}
+//	
+//	@Test
+//	void testRPD1LateNOK24HoursLate() { 
+//		TodoItem ti = new TodoItem("1", Instant.now().minus(24, ChronoUnit.HOURS), "desc item de 24h"); 
+//		// RG 1 : si l'item a 24h, le contenu de sa note ne contient pas "[LATE!]"
+//		assertFalse(ti.finalContent().contains("LATE"));
+//		
+//	}
 }
